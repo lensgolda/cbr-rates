635b9b1b243f54b6e2c09c9ee545445d3e75a702
Alexey Golda <lensgolda@gmail.com>
2019-04-29 15:24:49 +0200

    0.1.0 remove snapshot

--- project.clj
+++ project.clj
@@ -1,13 +1,12 @@
-(defproject cbr-rates "0.1.0-SNAPSHOT"
-  :description "Library for receiving currency rates on a certain date, 
-                from The Central Bank of the Russian Federation"
-  :url "https://github.com/lensgolda/cb-currency-rates"
+(defproject cbr-rates "0.1.0"
+  :description "Library for receiving currency rates on a certain date"
+  :url "https://github.com/lensgolda/cbr-rates"
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/data.xml "0.2.0-alpha5"]
                  [org.clojure/data.zip "0.1.2"]
-                 [clj-http "3.9.0"]
+                 [clj-http "3.9.1"]
                  [clj-time "0.15.1"]]
   :profiles {:dev {:source-paths ["dev"]
                    :dependencies [[org.clojure/clojure "1.10.0"]
